b96f9f895a53222c592e58cd6632a5649b3fe02e
Abhishek Kulkarni <100523970+abhigobi@users.noreply.github.com>
2023-04-30 23:12:46 +0530

    Create index.js

---
+++ Node-js-practice/index.js
@@ -0,0 +1,14 @@
+const superheroes = require('superheroes');
+
+
+var mySuperheroName = superheroes.random();
+
+console.log(mySuperheroName);
+
+const supervillains = require('supervillains');
+
+var superVillan = supervillains.random();
+
+
+
+//
